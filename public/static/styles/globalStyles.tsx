6d4dd89c87c4ea1d82abf2797f1e20e5c90c272b
RenasSitdikov <renas.sitdikov@flatstack.com>
2020-01-13 12:43:43 +0300

    added fonts

--- public/static/styles/globalStyles.tsx
+++ public/static/styles/globalStyles.tsx
@@ -1,5 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyles = createGlobalStyle``;
+const GlobalStyles = createGlobalStyle`
+  @import "../fonts/roboto.css";
+  body {
+    min-height: 100vh;
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', Helvetica, sans-serif;
+  }
+`;
 
 export default GlobalStyles;
