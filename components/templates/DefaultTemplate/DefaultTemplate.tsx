4a2e805c99f2b5fb0b61f54eb08cdbc370eed851
RenasSitdikov <renas.sitdikov@flatstack.com>
2020-01-18 23:50:46 +0300

    added type ReactNode

--- components/templates/DefaultTemplate/DefaultTemplate.tsx
+++ components/templates/DefaultTemplate/DefaultTemplate.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from '../../../public/static/styles/commonStyledComponents';
 import Header from '../../organisms/Header';
 
-const DefaultTemplate = ({ children }: any) => {
+const DefaultTemplate = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
