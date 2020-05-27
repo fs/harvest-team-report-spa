f132361f153764dc9d17855d20b66db097ad6304
RenasSitdikov <renas.sitdikov@flatstack.com>
2020-05-28 00:31:18 +0300

    Revert "Revert "changed server.js from next-base""
    This reverts commit d1d0198b0293c04ebe1264ee50df1b5be35cf53b.

--- server.js
+++ server.js
@@ -1,62 +1,29 @@
-const next = require('next');
-const routes = require('./routes');
-const express = require('express');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const port = 3000;
+const next = require('next');
+const routes = require('./routes');
+const express = require('express');
+const secure = require('express-force-https');
+
 const dev = process.env.NODE_ENV !== 'production';
-const availableRequestFormats = ['.css', '.js', '.json', '.map'];
+const port = parseInt(process.env.PORT, 10) || 3000;
+
 // Create the Express-Next App
 const app = next({ dev });
-const handle = app.getRequestHandler();
-
-function renderAndCache(req, res) {
-  const { route, params } = routes.match(req.url);
-
-  if (!route) {
-    const staticAsset = req.url.match(/^\/static\//);
-    const requestFormat = (req.url.match(/\.(\w+)/g) || []).pop();
-    if (staticAsset || !requestFormat || availableRequestFormats.includes(requestFormat)) {
-      return handle(req, res);
-    } else {
-      return res.status(404).send('Not Found');
-    }
-  }
-
-  const query = { ...req.query, ...params };
-
-  app
-    .renderToHTML(req, res, route.page, query)
-    .then(html => {
-      res.send(html);
-    })
-    .catch(err => {
-      app.renderError(err, req, res, route.page, params);
-    });
-}
-
+const handle = routes.getRequestHandler(app);
 // Start the app
 app
   .prepare()
   // Start Express server and serve the
   .then(() => {
-    const server = express();
-
-    server.use(renderAndCache).listen(port, err => {
-      if (err) throw err;
-      if (dev) {
-        console.log(`ready on http://localhost:${port}`);
-      } else {
-        console.log(`ready`);
-      }
-    });
-
-    server.get('/favicon.ico', (req, res) => {
-      app.serveStatic(req, res, __dirname + '/public/favicon.ico');
-    });
-    server.all('*', async (req, res) => {
-      return handle(req, res);
-    });
+    express()
+      .use(secure)
+      .use(handle)
+      .listen(port);
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
   });
