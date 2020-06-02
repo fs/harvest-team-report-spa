9d0affe01edd5ed6b2a02b8c3acb3edefd4a4e4d
RenasSitdikov <renas.sitdikov@flatstack.com>
2020-06-02 10:02:22 +0300

    changed settings

--- jest.config.js
+++ jest.config.js
@@ -7,20 +7,15 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFiles: ['<rootDir>/enzyme.config.js'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: [
-    '<rootDir>/.next/',
-    '<rootDir>/node_modules/',
-    '<rootDir>/.tests/helpers',
-    '<rootDir>/.legacy',
-  ],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/_tests_/helpers'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/.mocks/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/.mocks/styleMock.js',
-    '\\.svg': '<rootDir>/.mocks/svgrMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/_mocks_/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/_mocks_/styleMock.js',
+    '\\.svg': '<rootDir>/_mocks_/svgrMock.js',
   },
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/.tests/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testRegex: '(/_tests_/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
