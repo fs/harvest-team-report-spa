90db898a98c17ca0fb1af2e948ad4f727943f484
RenasSitdikov <renas.sitdikov@flatstack.com>
2020-06-25 15:28:17 +0300

    comment fix

--- public/styles/commonStyledVariables.tsx
+++ public/styles/commonStyledVariables.tsx
@@ -6,7 +6,8 @@ const { screenOnly, containerWidth } = theme;
 export const getContainer = (screen: string) => {
   return css`
     ${screenOnly(screen)} {
-      width: ${// @ts-ignore
+      width: ${//
+      // @ts-ignore
       containerWidth[screen]};
     }
   `;
