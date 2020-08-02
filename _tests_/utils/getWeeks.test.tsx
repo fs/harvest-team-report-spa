09ef546858f822720f9a3213c746b961b1ad35fa
RenasSitdikov <renas.sitdikov@flatstack.com>
2020-08-02 12:07:33 +0300

    new date test

--- _tests_/utils/getWeeks.test.tsx
+++ _tests_/utils/getWeeks.test.tsx
@@ -12,4 +12,17 @@ describe('getWeeksFromToDates', () => {
     // Assert
     expect(result).toEqual(expectedResult);
   });
+  test('getWeeksFromToDates should return data with week and year', () => {
+    // Arrange
+    const mockedWeek = '32';
+    const mockedYear = '2020';
+    const expectedResult = {
+      from: '2020-08-03',
+      to: '2020-08-09',
+    };
+    // Act
+    const result = getWeeksFromToDates(mockedWeek, mockedYear);
+    // Assert
+    expect(result).toEqual(expectedResult);
+  });
 });
