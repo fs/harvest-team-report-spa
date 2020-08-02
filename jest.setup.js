9611e78f36b57d8cb4e056f00bbd1722ad5f0c6e
RenasSitdikov <renas.sitdikov@flatstack.com>
2020-08-02 12:00:01 +0300

    fixed test

--- jest.setup.js
+++ jest.setup.js
@@ -1,11 +1,5 @@
+const MockDate = require('mockdate');
+
 jest.setTimeout(30000);
 
-const mockedDate = new Date(Date.UTC(2020, 5, 1, 0, 0, 0));
-const originalDate = Date;
-
-mockedDate.getTime = jest.fn(() => 1590969600000);
-global.Date.getFullYear = originalDate.getFullYear;
-global.Date.now = originalDate.now;
-global.Date.getTime = originalDate.getTime;
-global.Date.UTC = originalDate.UTC;
-global.Date.parse = originalDate.parse;
+MockDate.set(1590969600000);
